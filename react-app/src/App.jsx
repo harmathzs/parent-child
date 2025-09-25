d5db5045aea443163988e25d41918d3e4e33b4af
Zsolt Harmath <harmathzs@gmail.com>
2025-09-25 12:12:01 +0200

    Log

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -10,14 +10,34 @@ export default class App extends React.Component {
     name: 'App'
   }
 
+  handleChildHungry = child => {
+    console.log('handleChildHungry child, name', child, child.props.name)
+    // Why props and not state.name is Tim?
+  }
+
   render() {
     return (
       <>
         <div className="card">
           <Parent>
-            <Child name="Tim" parent={this}>child content text</Child>
-            <Child name="John" parent={this}>child content text</Child>
-            <Child name="Meg" parent={this}>child content text</Child>
+            <Child 
+              name="Tim" 
+              parent={this} 
+              onHungry={this.handleChildHungry}>
+                child content text
+            </Child>
+            <Child 
+              name="John" 
+              parent={this} 
+              onHungry={this.handleChildHungry}>
+                child content text
+            </Child>
+            <Child 
+              name="Meg" 
+              parent={this} 
+              onHungry={this.handleChildHungry}>
+                child content text
+            </Child>
             <p>paragraph content</p>
             parent content text
           </Parent>
--- react-app/src/components/Child/Child.jsx
+++ react-app/src/components/Child/Child.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 export default class Child extends React.Component {
+    handleButtonClick = me => {
+        this.props.onHungry(me)
+    }
+
     render() {
         return (
             <div>
-                <p>Child {this.props.name}</p>
-                <p>My parent is: {this.props.parent?.state.name}</p> {/* App ?? */}
+                <p>
+                    Child {this.props.name}; my parent is: {this.props.parent?.state.name}.
+                    <button onClick={()=>this.handleButtonClick(this)}>I'm hungry</button>
+                </p> {/* App ?? */}
             </div>
         )
     }
